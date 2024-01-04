a3e3542b6073069c36321bb740b7393d77fe650a
Muhammed Hadhil <hadhilnjrbrototype@gmail.com>
2024-01-04 16:44:35 +0530

    Challenge completed

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    bool releaseDog = true;
+
+    private void Start()
+    {
+        InvokeRepeating("TimeDelay", 2, 2);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (releaseDog)
+        {
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                releaseDog = false;
+            }
+
+        }
+    }
+    void TimeDelay()
+    {
+        releaseDog = !releaseDog;
+    }
+}
